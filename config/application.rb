783479e6ce999dc51d754501309f121e6544aeaa
yoshikiryuno <a.y.football9615@gmail.com>
2020-09-26 16:32:23 +0900

    commit

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module ChatSpace
   class Application < Rails::Application
     config.load_defaults 6.0
     config.generators do |g|
+    config.i18n.default_locale = :ja
       g.stylesheets false
       g.javascripts false
       g.helper false
